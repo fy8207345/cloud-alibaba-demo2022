28c120a03b74688e5ec773789acf93e44c9d378c
Feng Yan <2316696377.com>
2022-02-06 13:31:12 +0800

    aaa

--- circuitbreaker-sentinel/src/main/java/com/fy/sentinel/FeignCiucuitBreakerApplication.java
+++ circuitbreaker-sentinel/src/main/java/com/fy/sentinel/FeignCiucuitBreakerApplication.java
@@ -2,13 +2,21 @@ package com.fy.sentinel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.archaius.ArchaiusAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ArchaiusAutoConfiguration.class})
 @EnableFeignClients
+@EnableDiscoveryClient
 public class FeignCiucuitBreakerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FeignCiucuitBreakerApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FeignCiucuitBreakerApplication.class, args);
+        String value = applicationContext.getEnvironment().getProperty("feign.circuitbreaker.enabled");
+        String value2 = applicationContext.getEnvironment().getProperty("feign.sentinel.rules.default[0].timeWindow");
+        System.out.println(value);
+        System.out.println(value2);
     }
 }
--- circuitbreaker-sentinel/src/main/java/com/fy/sentinel/controller/TestController.java
+++ circuitbreaker-sentinel/src/main/java/com/fy/sentinel/controller/TestController.java
@@ -3,17 +3,22 @@ package com.fy.sentinel.controller;
 import com.fy.sentinel.feign.OrderClient;
 import com.fy.sentinel.feign.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
 
     @Autowired
     private UserClient userClient;
     @Autowired
     private OrderClient orderClient;
+    @Value("${feign.circuitbreaker.enabled}")
+    private boolean enabled;
 
     @GetMapping("/test/default/{ok}")
     public String testDefault(@PathVariable boolean ok) {
@@ -28,5 +33,10 @@ public class TestController {
     @GetMapping("/test/feignMethod/{ok}")
     public String testFeignMethod(@PathVariable boolean ok) {
         return userClient.feignMethod(ok);
+    }
+
+    @GetMapping("/enabled")
+    public boolean isEnabled(){
+        return enabled;
     }
 }
